451a7c15f3240abffa30388764102ebdae9d13bf
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-12-03 17:27:57 -0800

    connecting mongodb atlas database

--- server/index.js
+++ server/index.js
@@ -9,3 +9,11 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
 
+const CONNECTION_URL = 'mongodb+srv://EdwardVilla0:Eddlover0@cluster0.ylblg.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log('Server Running')))
+    .catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify', false);
