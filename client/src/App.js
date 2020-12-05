db5b3022c6a8b9c3ca61c3e3c6d316f73cbd53e1
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-12-05 12:11:19 -0800

    setting up post component and importing material ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import memores from './images/memories.png';
 
 function App() {
     return (
-        <div>
-            <h1>app</h1>
-        </div>
+        <Container maxidth='lg'>
+            <AppBar position='static' color='inherit'>
+                <Typography variant='h2' align='center' >Memories</Typography>
+                <img src={memories} alt="memories" height="60" />
+            </AppBar>
+        </Container>
     )
 }
 
