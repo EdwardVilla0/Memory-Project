6e0aef4ab82b118882580db704bfda12193dc97d
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-12-08 14:28:28 -0800

    using redux in post and altering the package.json

--- client/src/components/Posts/Posts.component.js
+++ client/src/components/Posts/Posts.component.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Post from './Post/Post.component';
+import { useSelector } from 'react-redux';
 import memories from '../../images/memories.png';
 import useStyles from './Posts.styles';
 
 const Posts = () => {
+    const posts = useSelector((state) => state.posts);
     const classes = useStyles();
 
     return (
