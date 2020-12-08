7f720ae76c392752da966cc17f35797e7eb5372a
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-12-08 12:12:01 -0800

    setting up reducer for redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+import { getPosts } from './actions/posts.actions';
 import Posts from './components/Posts/Posts.component';
 import Form from './components/Form/Form.component';
 import memories from './images/memories.png';
@@ -8,6 +11,11 @@ import useStyles from './styles';
 
 function App() {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
 
     return (
         <Container maxidth='lg'>
