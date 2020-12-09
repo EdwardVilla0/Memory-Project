c002f4f1b403b8fca0fa89fe98085f0507dbd39d
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-12-08 16:53:51 -0800

    sending post and using api

--- client/src/components/Form/Form.component.js
+++ client/src/components/Form/Form.component.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 import { TextField, Button, Typography, Paper } from '@material-ui/core'
 import FileBase from 'react-file-base64';
+import { useDispatch } from 'react-redux';
 import useStyles from './Form.styles';
+import { createPost } from '../../actions/posts.actions';
 
 
 const Form = () => {
@@ -14,8 +16,11 @@ const Form = () => {
             selectedFile: ''
         });
     const classes = useStyles();
-    const handleSubmit = () => {
+    const dispatch = useDispatch();
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(createPost)
     }
 
     const clear = () => {
@@ -24,7 +29,7 @@ const Form = () => {
 
     return (
         <Paper className={classes.paper}>
-            <form autoComplete="off" noValidate className={`${classes.root}`} onSubmit={handleSubmit}>
+            <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
                 <Typography variant="h6">Creating a Memory</Typography>
                 <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
                 <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
