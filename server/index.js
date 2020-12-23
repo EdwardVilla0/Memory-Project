4e29a393a356379137a6fd1a2d5f3c8b51ed9928
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-12-22 18:48:00 -0800

    fixing start bug

--- server/index.js
+++ server/index.js
@@ -14,7 +14,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://EdwardVilla0:Eddlover0@cluster0.ylblg.mongodb.net/<dbname>?retryWrites=true&w=majority';
+app.get('/', (req, res) => {
+    res.send('Hello to memories API');
+})
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
